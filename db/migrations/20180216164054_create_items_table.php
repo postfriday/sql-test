7089e98dc88eee126b8aa58aed87e86196c9b8a8
Stanislav Surskiy <ssurskiy@gmail.com>
2018-02-16 20:11:13 +0300

    Adds database migration & seeder

---
+++ db/migrations/20180216164054_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateItemsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('items');
+        $table->addColumn('category_id', 'integer', ['limit' => 11, 'signed' => false])
+            ->addColumn('name', 'string', ['limit' => 50])
+            ->addColumn('price', 'decimal', ['precision' => 10, 'scale' => 2, 'signed' => false])
+            ->addColumn('created', 'datetime')
+            ->addColumn('updated', 'datetime', ['null' => true])
+            ->create();
+    }
+}
---
+++ db/seeds/ItemsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class ItemsSeeder extends AbstractSeed
+{
+    private $tablename = 'items';
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 100; $i++) {
+            $data[] = [
+                'category_id'   => rand(1, 5),
+                'name'          => $faker->realText($faker->numberBetween(20, 50)),
+                'price'         => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 1000),
+                'created'       => date('Y-m-d H:i:s'),
+            ];
+        }
+        $this->insert($this->tablename, $data);
+    }
+}
