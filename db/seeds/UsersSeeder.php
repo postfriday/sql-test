84ca2efd14e8e54824a51f574579b9410f5fcaef
Stanislav Surskiy <ssurskiy@gmail.com>
2018-02-16 21:28:28 +0300

    Adds migrations & seeder for table `users`

---
+++ db/seeds/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UsersSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $genders = ['male' => 'M', 'female' => 'F'];
+        $data = [];
+        for ($i = 0; $i < 100; $i++) {
+            $gender = array_keys($genders)[random_int(0, 1)];
+            $data[] = [
+                'name'      => $name = $faker->name($gender),
+                'gender'    => $genders[$gender],
+                'birthdate' => $faker->date($format = 'Y-m-d', $max = '-5 years')
+            ];
+        }
+        $this->insert('users', $data);
+
+    }
+}
