90ef92ac2cf3e6517dc61a3d4d7d2fed71e3bf65
Stanislav Surskiy <ssurskiy@gmail.com>
2018-02-16 21:28:40 +0300

    Adds migrations & seeder for table `users_banned`

---
+++ db/seeds/UsersBannedSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UsersBannedSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $users = [0];
+        for ($i = 0; $i < 20; $i++) {
+            do {
+                $user_id = rand(1, 100);
+            } while (in_array($user_id, $users));
+            $users[] = $user_id;
+            $data[] = [
+                'user_id'   => $user_id
+            ];
+        }
+        $this->insert('users_banned', $data);
+
+    }
+}
